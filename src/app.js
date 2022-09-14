4ce54cd86824dc1677da6c08b6f567d43ba9cf61
abhi9882 <100504872+abhi9882@users.noreply.github.com>
2022-09-14 20:54:56 +0530

    added-css

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Grammar from './components/Grammar.js';
+import Upload from './components/Upload.js';
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>
+      <h1>Prowriter</h1>
+      <p>Prowriter helps you write mistake-free in Gmail, 
+        Facebook, Twitter, LinkedIn, and any other app you use. 
+        Even in text messages! Write Better in Seconds.</p>
+      </div>
+
+        <Grammar/>
+
+
+        <Upload/>
+
+        <div id="disclaimer">Copyright © 2022 Developed by SufranKhan for Daten and Wissen Pvt. Ltd.
+    </div>
+    </div>
+  );
+}
+
+export default App;
